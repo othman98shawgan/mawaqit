b17f0d08ed0f09165ceefb3eb9208f76ea71f743
Othman Shawgan <oth1998@gmail.com>
2022-06-08 18:58:43 +0300

    Remove Duration

--- lib/ui/widgets/clock_widget.dart
+++ lib/ui/widgets/clock_widget.dart
@@ -44,22 +44,13 @@ class AfterClockWidget extends StatelessWidget {
   const AfterClockWidget({Key? key, required this.time}) : super(key: key);
   final DateTime time;
 
-  String printDuration(Duration duration) {
-    return "${addZero(duration.inHours.toString())}:"
-        "${addZero(duration.inMinutes.remainder(60).toString())}:"
-        "${addZero(duration.inSeconds.remainder(60).toString())}";
-  }
-
-  String addZero(String time) {
-    if (time.length == 2) return time;
-    return '0$time';
-  }
-
   @override
   Widget build(BuildContext context) {
     return TimerBuilder.periodic(const Duration(seconds: 1), builder: (context) {
+      var now = DateTime.now();
       return Text(
-        printDuration(time.difference(DateTime.now())),
+        DateFormat('kk:mm:ss').format(
+            time.subtract(Duration(hours: now.hour, minutes: now.minute, seconds: now.second))),
         style: const TextStyle(
           fontWeight: FontWeight.w300,
           fontSize: 32,
