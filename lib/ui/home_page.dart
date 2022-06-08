04c6bfcaccfb567f1dc363acba37a8269c65d3df
Othman Shawgan <oth1998@gmail.com>
2022-06-08 16:26:50 +0300

    Create clock widget file

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -8,6 +8,7 @@ import 'package:jiffy/jiffy.dart';
 import '../prayer_times.dart';
 import '../prayers.dart';
 import 'widgets/card_widget.dart';
+import 'widgets/clock_widget.dart';
 import 'widgets/prayer_widget.dart';
 
 class Prayer {
@@ -182,72 +183,5 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ),
     );
-  }
-}
-
-class ClockWidget extends StatelessWidget {
-  const ClockWidget({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return TimerBuilder.periodic(const Duration(seconds: 1), builder: (context) {
-      return Text(
-        DateFormat('kk:mm:ss').format(DateTime.now()),
-        style: const TextStyle(
-          fontWeight: FontWeight.w300,
-          fontSize: 48,
-        ),
-      );
-    });
-  }
-}
-
-class BeforeClockWidget extends StatelessWidget {
-  const BeforeClockWidget({Key? key, required this.time}) : super(key: key);
-  final DateTime time;
-
-  @override
-  Widget build(BuildContext context) {
-    return TimerBuilder.periodic(const Duration(seconds: 1), builder: (context) {
-      return Text(
-        DateFormat('kk:mm:ss')
-            .format(DateTime.now().subtract(Duration(hours: time.hour, minutes: time.minute))),
-        style: const TextStyle(
-          fontWeight: FontWeight.w300,
-          fontSize: 32,
-        ),
-      );
-    });
-  }
-}
-
-class AfterClockWidget extends StatelessWidget {
-  const AfterClockWidget({Key? key, required this.time}) : super(key: key);
-  final DateTime time;
-
-  String printDuration(Duration duration) {
-    return "${addZero(duration.inHours.toString())}:"
-        "${addZero(duration.inMinutes.remainder(60).toString())}:"
-        "${addZero(duration.inSeconds.remainder(60).toString())}";
-  }
-
-  String addZero(String time) {
-    if (time.length == 2) return time;
-    return '0$time';
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return TimerBuilder.periodic(const Duration(seconds: 1), builder: (context) {
-      return Text(
-        printDuration(time.difference(DateTime.now())),
-        style: const TextStyle(
-          fontWeight: FontWeight.w300,
-          fontSize: 32,
-        ),
-      );
-    });
   }
 }
