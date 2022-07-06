93c08a0dc5b6b539c54739a1b614a8aff47c3c56
Othman Shawgan <oth1998@gmail.com>
2022-07-06 20:02:05 +0300

    #2

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,6 @@ class MyApp extends StatelessWidget {
         '/home': (context) => const MyHomePage(
               title: 'Alfajr',
             ),
-        '/counter': (context) => const CounterPage(),
       },
       home: Container(
         decoration: const BoxDecoration(
